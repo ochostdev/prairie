3aaf97b11ba1899bb1b436c459dbb841755d4597
ct <ct@ocloud.com>
2018-06-20 15:45:04 +0800

    update slavenode multi-task support

--- slavenode/client_slave.py
+++ slavenode/client_slave.py
@@ -3,15 +3,31 @@ import psutil
 import subprocess
 import time
 import os
+import subprocess
+import paramiko
+import hashlib
 import threading
+
+os.system("docker rm -f $(docker ps -aq)")
+resc = {}
+resc['cpu'] = resc['gpu'] = resc['mem'] = resc['disk'] = 0
 
 def total():
     msg={}
     msg["uuid"]=uuid
     msg["cpu"]=psutil.cpu_count()
     msg["gpu"]=1 if subprocess.getstatusoutput('nvidia-smi')[0]==0 else 0
-    msg["mem"]=round((psutil.virtual_memory().available)/(1024*1024*1024),2)
-    #msg["disk"]=round((psutil.disk_usage('/').free)/(1024*1024*1024),2)
+    msg["mem"]=round((psutil.virtual_memory().total)/(1024*1024*1024), 2)
+    msg["disk"]=round((psutil.disk_usage('/').available)/(1024*1024*1024), 2)
+#    msg["rdisk"]=round((psutil.disk_usage('/').free)/(1024*1024*1024), 2)
+#    msg["rmem"]=round((psutil.virtual_memory().available)/(1024*1024*1024), 2)
+    
+    msg["mem"]=round(msg["mem"]*0.8, 2)
+
+    msg["rdisk"]=float(msg["disk"])-float(resc['disk'])
+    msg["rmem"]=float(msg["mem"])-float(resc['mem'])
+    msg["rgpu"]=int(msg["gpu"])-int(resc['gpu'])
+    msg["rcpu"]=int(msg["cpu"])-int(resc['cpu'])
     return str(msg)
 
 def clients(msg):
@@ -36,12 +52,20 @@ class myThread(threading.Thread):
         self.taskid=ret.get('_id')
         self.name=ret.get('dockername1')
         self.cmd=ret.get('cmd1')
-        self.username=ret.get('username','ocloud')
-        self.password=ret.get('password','ohcloud@123')
-        self.remote=ret.get('remote','18.18.110.220:5000')
-        self.runtime=ret.get('renttime','60')
+        self.username=ret.get('username', 'ocloud')
+        self.password=ret.get('password', 'ohcloud@123')
+        self.remote=ret.get('remote', '18.18.110.220:5000')
+        self.runtime=ret.get('renttime', '60')
+        self.cpu=int(ret.get('cpu', '1'))
+        self.gpu=int(ret.get('gpu', '0'))
+        self.mem=float(ret.get('mem', '0'))
+        self.disk=float(ret.get('disk', '0'))
 
     def run(self):
+        resc['cpu'] = resc['cpu']+self.cpu
+        resc['gpu'] = resc['gpu']+self.gpu
+        resc['mem'] = resc['mem']+self.mem
+        resc['disk'] = resc['disk']+self.disk
         if self.taskid and self.name and self.cmd:
             ret=self.pull_run(self.name,self.cmd,self.username,self.password,self.remote)
             if ret==0:
@@ -53,6 +77,11 @@ class myThread(threading.Thread):
         else:
             self.clients('recv message error')
         print('thread1 finish')
+        resc['cpu'] = resc['cpu']-self.cpu
+        resc['gpu'] = resc['gpu']-self.gpu
+        resc['mem'] = resc['mem']-self.mem
+        resc['disk'] = resc['disk']-self.disk
+
 
     def pull_run(self,name,cmd,username,password,remote):
         login=subprocess.getoutput('docker login -u %s -p %s %s'%(username,password,remote))
@@ -111,6 +140,7 @@ class myThread(threading.Thread):
 
 if __name__=="__main__":
     uuid='ct'
+    #host='116.62.129.207'
     host='18.18.110.220'
     port=12354
     addr=(host,port)
